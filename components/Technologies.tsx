fa91ebcaf0ae694d2b7dc98cd8a0f5a41364b5cf
Konrad <117399047+PandakiraDev@users.noreply.github.com>
2023-11-23 21:45:40 +0100

    fixes

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { TechnologiesListQuery } from '../generated/graphql';
-import Image from 'next/image';
+import React from "react";
+import { TechnologiesListQuery } from "../generated/graphql";
+import Image from "next/image";
 
 type Props = {
-  technologies: TechnologiesListQuery['technologies'];
+  technologies: TechnologiesListQuery["technologies"];
 };
 
 export const Technologies = ({ technologies }: Props) => {
@@ -11,7 +11,16 @@ export const Technologies = ({ technologies }: Props) => {
     <section className="flex flex-col sm:flex-row justify-center items-center md:justify-evenly w-[90%] my-10 space-y-8 md:space-x-8 md:space-y-0 p-8 bg-white rounded-3xl mx-auto">
       {technologies.map((item) => (
         <div key={item.id} className="w-[200px]">
-          <Image src={item.image.url} layout="responsive" width={4} height={3} objectFit="contain" alt="technologia" />
+          {item.image?.url ? (
+            <Image
+              src={item.image.url}
+              layout="responsive"
+              width={4}
+              height={3}
+              objectFit="contain"
+              alt="technologia"
+            />
+          ) : null}
         </div>
       ))}
     </section>
